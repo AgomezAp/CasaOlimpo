5f4abe2c96e630911db10fdecaef7baf08ae1c3a
Inge-AP <aagudelo.investigacion@andrespublicidadtg.com>
2025-05-08 11:47:29 -0500

    mensajeria cron

--- Backend/src/routes/mensajeria.ts
+++ Backend/src/routes/mensajeria.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { enviarMensaje, obtenerFecha, obtenerMensaje, mensajeToFront } from '../controllers/mensajeria';
+import { enviarMensaje, obtenerFecha, obtenerMensaje, mensajeToFront, serverwsocket, verificarSesion, eliminarSesion, nuevaSesion } from '../controllers/mensajeria';
 
 const router = Router();
 router.post("/api/mensajeria/felicitaciones", enviarMensaje);
 router.post("/api/mensajeria/obtenerMensaje", obtenerMensaje);
 router.get("/api/mensajeria/obtenerCumple", obtenerFecha);
-router.get("/api/mensajeria/mensaje", mensajeToFront)
+router.get("/api/mensajeria/mensaje", mensajeToFront);
+router.get("/api/mensajeria/session", verificarSesion);
+router.post("/api/mensajeria/nuevaSesion", serverwsocket)
+router.delete("/api/mensajeria/eliminarSesion", eliminarSesion);
 export default router;
